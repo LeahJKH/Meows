0c08bd3b57081d9d3403d627103faa779d87627e
FeyzaO-H04 <feyzaoh04.kodehode@gmail.com>
2023-12-13 10:51:31 +0100

    created html for discover page

--- src/Pages/DiscoverPage/discoverPage.tsx
+++ src/Pages/DiscoverPage/discoverPage.tsx
@@ -5,7 +5,41 @@ export function Discover(){
   return(
     <div className={styles.discoverContainer}>
 
-      <h1>Discover</h1>
+      <h1>DISCOVER</h1>
+
+      <div className={styles.boxContainer}>
+        <div className={styles.boxesLeftContainer}>
+          <div className={styles.boxes}>
+            <h3>TOP MEOW THIS WEEK</h3>
+            <div></div>
+          </div>
+          <div className={styles.boxes}>
+            <h3>TOP ALL TIME</h3>
+            <div></div>
+          </div>
+          <div className={styles.boxes}>
+            <h3>TOP USERS</h3>
+            <div></div>
+          </div>
+        </div>
+
+        <div className={styles.boxesRightContainer}>
+          <div className={styles.boxes}>
+            <h3>TRENDING TOPICS</h3>
+            <div></div>
+          </div>
+          <div className={styles.boxes}>
+            <h3>HASHTAGS</h3>
+            <div></div>
+          </div>
+          <div className={styles.boxes}>
+            <h3>USERS</h3>
+            <div></div>
+          </div>
+        </div>
+
+
+      </div>
 
     </div>
   )
