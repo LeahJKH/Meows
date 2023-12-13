0489ebb5381a6136cd9cbf0e36e2b4b09129afbe
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-12-13 12:29:48 +0100

    Fixed error

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,11 +1,23 @@
 import MeowsLogo from "../../assets/MeowsLogo";
 import styles from "./NavBar.module.css";
+import MoonSvg from "../../assets/Icons/Moon";
+import SunSvg from "../../assets/Icons/Sun";
+import Searchbar from "../srcbar/SrcBar";
+import LogoutSvg from "../../assets/Icons/Logout";
 
 export default function NavBar() {
   return (
     <>
       <nav className={styles.navBar}>
-        <MeowsLogo />
+        <div className={styles.leftNav}>
+          <MeowsLogo />
+          <MoonSvg />
+          <SunSvg />
+        </div>
+        <div className={styles.rightNav}>
+          <button>Search.....</button>
+          <LogoutSvg />
+        </div>
       </nav>
     </>
   );
