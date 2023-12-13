013ad44cc6854e2714e3ee7b4926ffa801b9fc78
LeahJKH <leahjb06.kodehode@gmail.com>
2023-12-13 10:22:07 +0100

    did some shit

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import PostCard from "../Components/PostCard/Postcard.tsx";
 import Searchbar from "../Components/srcbar/SrcBar";
 import {PostInfo}  from "../Data/posts.ts";
-
+import Signup from "../Components/Signup/Signup.tsx";
 
 export function LandingPage() {
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -13,7 +13,7 @@ export function LandingPage() {
 
   return (
     <main>
-    
+    <Signup/>
       <Searchbar setSearchTerm={setSearchTerm}></Searchbar>
       {filteredPosts.map((post: { id: number, title: string, content: string }) => (
         <PostCard key={post.id} title={post.title} content={post.content} />
