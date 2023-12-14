3cbc7298a4d8eb289b8da6cd14b58f58a94bb596
LeahJKH <leahjb06.kodehode@gmail.com>
2023-12-14 13:08:54 +0100

    doing some more fixes in styling

--- src/Pages/DiscoverPage/discoverPage.tsx
+++ src/Pages/DiscoverPage/discoverPage.tsx
@@ -1,14 +1,15 @@
 import styles from "./discoverPage.module.css";
 import PostCard from "../../Components/PostCard/Postcard";
 import { PostInfo } from "../../Data/posts";
+import FireSvg from "../../assets/Icons/Fire";
 
 export function Discover() {
   return (
     <div className={styles.discoverContainer}>
 
       <h1>DISCOVER</h1>
-
-          <div >
+<div className={styles.topside}>
+          <div className={styles.MeowBox}>
             <h3>TOP MEOW THIS WEEK</h3>
             <div className={styles.MeowContainer}>
               <PostCard  {...PostInfo[1]}/> 
@@ -17,15 +18,48 @@ export function Discover() {
             </div>
           </div>  
          
-          <div className={styles.boxes}>
+          <div className={styles.HashTagBox}>
             <h3>HASHTAGS</h3>
-            <div></div>
+            <div className={styles.HashTagContain}>
+
+              <div className={styles.HashS}>
+                <FireSvg />
+                <p>#Cats</p>
+              </div>
+
+              <div className={styles.HashS}>
+              <FireSvg />
+                <p>#Paris</p>
+              </div>
+
+              <div className={styles.HashS}>
+              <FireSvg />
+                <p>#Astrology</p>
+              </div>
+
+              <div className={styles.HashS}>
+              <FireSvg />
+                <p>#Dream</p>
+              </div>
+
+              <div className={styles.HashS}>
+              <FireSvg />
+                <p>#Vocaloid</p>
+              </div>
+
+              <div className={styles.HashS}>
+              <FireSvg />
+                <p>#space</p>
+              </div>
+            </div>
+          </div>
           </div>
 
-          <div className={styles.boxes}>
+          <div className={styles.bottomReco}>
             <h3>Recomended</h3>
             <div>
-              <h1></h1>
+              <img src="./Alicia.jpg" alt="" />
+              <h4>user</h4>
             </div>
           </div>
        
