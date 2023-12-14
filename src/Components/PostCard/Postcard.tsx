635db01356ce5e50a19af5257d7b5a8df9c2dff4
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-12-14 13:10:17 +0100

    css

--- src/Components/PostCard/Postcard.tsx
+++ src/Components/PostCard/Postcard.tsx
@@ -3,7 +3,6 @@ import styles from "./Postcard.module.css";
 import HeartSvg from "../../assets/Icons/Heart";
 import SavedSvg from "../../assets/Icons/Saved";
 
-
 type PostCardProps = {
   title: string;
   content: string;
@@ -11,7 +10,6 @@ type PostCardProps = {
 
 const PostCard: React.FC<PostCardProps> = ({ title, content }) => {
   return (
-
     <div className={styles.postCardContainer}>
       <div className={styles.postCard}>
         <img
