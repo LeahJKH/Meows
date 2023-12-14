ef1bca4514e9b36f227b31a0dc72b2c3824127a3
LeahJKH <leahjb06.kodehode@gmail.com>
2023-12-14 12:17:22 +0100

    started on discovery page

--- src/Pages/DiscoverPage/discoverPage.tsx
+++ src/Pages/DiscoverPage/discoverPage.tsx
@@ -1,4 +1,6 @@
 import styles from "./discoverPage.module.css";
+import PostCard from "../../Components/PostCard/Postcard";
+import { PostInfo } from "../../Data/posts";
 
 export function Discover() {
   return (
@@ -6,40 +8,27 @@ export function Discover() {
 
       <h1>DISCOVER</h1>
 
-      <div className={styles.boxContainer}>
-        <div className={styles.boxesLeftContainer}>
-          <div className={styles.boxes}>
+          <div >
             <h3>TOP MEOW THIS WEEK</h3>
-            <div></div>
-          </div>
-          <div className={styles.boxes}>
-            <h3>TOP ALL TIME</h3>
-            <div></div>
-          </div>
-          <div className={styles.boxes}>
-            <h3>TOP USERS</h3>
-            <div></div>
-          </div>
-        </div>
-
-        <div className={styles.boxesRightContainer}>
-          <div className={styles.boxes}>
-            <h3>TRENDING TOPICS</h3>
-            <div></div>
-          </div>
+            <div className={styles.MeowContainer}>
+              <PostCard  {...PostInfo[1]}/> 
+              <PostCard {...PostInfo[3]}/>
+              <PostCard {...PostInfo[12]}/>
+            </div>
+          </div>  
+         
           <div className={styles.boxes}>
             <h3>HASHTAGS</h3>
             <div></div>
           </div>
+
           <div className={styles.boxes}>
-            <h3>USERS</h3>
-            <div></div>
+            <h3>Recomended</h3>
+            <div>
+              <h1></h1>
+            </div>
           </div>
-        </div>
-
-
+       
       </div>
-
-    </div>
   );
 }
