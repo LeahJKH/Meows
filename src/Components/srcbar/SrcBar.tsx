740734905b1afd2de81e858eb14199e7b6cf3a01
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-12-15 11:29:57 +0100

    navbar og search

--- src/Components/srcbar/SrcBar.tsx
+++ src/Components/srcbar/SrcBar.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-
+import styles from "../NavBar/NavBar.module.css";
 interface SearchbarProps {
   setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
-}// This sets the search term in a variable we can drag out later
+} // This sets the search term in a variable we can drag out later
 
 const Searchbar = ({ setSearchTerm }: SearchbarProps) => {
   const [searchValue, setSearchValue] = useState(""); //this either switches from empty or a word
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
-  };// this reads the letters put in the input field
+  }; // this reads the letters put in the input field
 
   const handleSearchClick = () => {
     setSearchTerm(searchValue);
@@ -19,7 +19,7 @@ const Searchbar = ({ setSearchTerm }: SearchbarProps) => {
     if (e.key === "Enter") {
       setSearchTerm(searchValue);
     }
-  };//this makes so you can click the enter button on your keyboard and it searches
+  }; //this makes so you can click the enter button on your keyboard and it searches
 
   return (
     <div>
@@ -29,7 +29,9 @@ const Searchbar = ({ setSearchTerm }: SearchbarProps) => {
         onChange={handleSearchChange}
         onKeyDown={handleKeyDown}
       />
-      <button onClick={handleSearchClick}>Search</button>
+      <button className={styles.searchButton} onClick={handleSearchClick}>
+        Search
+      </button>
     </div>
   );
 };
